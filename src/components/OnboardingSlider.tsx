07d6e05727b8904121f416369b0c078f81498fa6
chrismbah <mbahchris46@gmail.com>
2024-05-31 15:36:02 +0100

    add: onboarding carousel

---
+++ src/components/OnboardingSlider.tsx
@@ -0,0 +1,44 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { onboardingSlides } from "../data/onboarding";
+export const OnboardingSlider = () => {
+  const settings = {
+    dots: false,
+    arrows: false,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider {...settings} className="h-[584px] w-[436px]">
+      {onboardingSlides.map(({ img, title, desc }, i) => (
+        <div
+          key={i}
+          className="flex items-center justify-center w-full gap-12 "
+        >
+          <div className="w-full flex items-center justify-center">
+            <img
+              src={img}
+              alt={title}
+              className="w-[300px] h-[300px] object-cover "
+            />
+          </div>
+
+          <div className="flex items-center flex-col justify-center w-full gap-[12px] ">
+            <h1 className="font-bold text-[40px] leading-[48px] text-center text-white">
+              {title}
+            </h1>
+            <p className="font-medium text-[18px] leading-[28px] text-center text-[#F8D4D2] ">
+              {desc}
+            </p>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  );
+};
---
+++ src/data/onboarding.ts
@@ -0,0 +1,26 @@
+import onboarding1 from "../assets/svg/onboarding/onboarding1.svg";
+import onboarding2 from "../assets/svg/onboarding/onboarding2.svg";
+import onboarding3 from "../assets/svg/onboarding/onboarding3.svg";
+import onboarding4 from "../assets/svg/onboarding/onboarding4.svg";
+export const onboardingSlides = [
+  {
+    img: onboarding1,
+    title: "Welcome to EnterpriseBloom",
+    desc: "Discover a community where marketing, creativity, technology, and AI ideas meet from all over the world for business growth. Let's get started!",
+  },
+  {
+    img: onboarding2,
+    title: "Explore Inspiring Campaigns",
+    desc: "Browse through a diverse collection of marketing campaigns, each with case studies and results. ",
+  },
+  {
+    img: onboarding3,
+    title: "Share Your Success Story",
+    desc: "Upload your marketing campaigns and showcase your strategies, creative executions, and results ",
+  },
+  {
+    img: onboarding4,
+    title: "Enter The world no.1 Blooming Community of ideas.",
+    desc: "Upload your marketing campaigns and showcase your strategies, creative executions, and results ",
+  },
+];
