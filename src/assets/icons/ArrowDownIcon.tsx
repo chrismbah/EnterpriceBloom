0be1e8de6ba57c147af2f8f1ee3fcf7c81dfd4ef
chrismbah <mbahchris46@gmail.com>
2024-06-03 16:12:46 +0100

    add: arrow icon for select input

---
+++ src/assets/icons/ArrowDownIcon.tsx
@@ -0,0 +1,19 @@
+export const ArrowDownIcon = () => {
+  return (
+    <div>
+      <svg
+        className="w-4 h-4 text-neutral-700"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </div>
+  );
+};
