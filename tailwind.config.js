37d7b418c82fbfd779f5c3035be1b941f51af752
chrismbah <mbahchris46@gmail.com>
2024-06-03 16:29:41 +0100

    fix: add tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
       white: "#ffffff",
