6dbebe53b7642f9cfa40c3508e61a14755fd1a40
chrismbah <mbahchris46@gmail.com>
2024-06-05 13:18:57 +0100

    refactor: carousel file location

--- src/components/carousel/OnboardingSlider.tsx
+++ src/components/carousel/OnboardingSlider.tsx
@@ -1,7 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { onboardingSlides } from "../data/onboarding";
+import { onboardingSlides } from "../../data/onboarding";
 export const OnboardingSlider = () => {
   const settings = {
     dots: false,
