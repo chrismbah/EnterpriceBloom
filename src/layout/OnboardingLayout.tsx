ed7df21baed5559b0973d924a77db8e0fea55e7a
chrismbah <mbahchris46@gmail.com>
2024-06-07 10:12:52 +0100

    refactor: onbarding layout slider responsiveness

--- src/layout/OnboardingLayout.tsx
+++ src/layout/OnboardingLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo_white.png";
 import { OnboardingSlider } from "../components/carousel/OnboardingSlider";
 const OnboardingLayout = () => {
   return (
     <div className="onboarding-layout flex min-h-screen w-screen overflow-x-hidden ">
       <div
-        className="slider-section w-[40%] bg-primary-500 min-h-screen z-10
+        className="slider-section hidden mlg:block mlg:w-[40%] bg-primary-500 min-h-screen z-10
           lg:flex items-center flex-col justify-center"
       >
         <img
@@ -17,7 +17,7 @@ const OnboardingLayout = () => {
       </div>
       <div
         className="h-screen overflow-y-auto hide-scrollbar overflow-x-hidden 
-        w-[60%] flex flex-col pt-[68px] pb-[45px] px-[50px]"
+         w-full mlg:w-[60%] flex flex-col pt-[68px] pb-[45px] px-[36px] lg:px-[50px]"
       >
         <Outlet />
       </div>
