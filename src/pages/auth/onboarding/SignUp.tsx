a9bf4f19c7be4fb82e53a4a1c69698d3bc773c80
chrismbah <mbahchris46@gmail.com>
2024-06-07 10:14:36 +0100

    style: increase width of signup form

--- src/pages/auth/onboarding/SignUp.tsx
+++ src/pages/auth/onboarding/SignUp.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import SignUpForm from "./SignUpForm.";
 const SignUp = () => {
   return (
-    <div className="w-[80%]">
+    <div className=" w-[90%] lg:w-[85%] xl:w-[80%]">
       <div className="mb-[28px] ">
         <h1 className="font-bold text-3xl mb-[5px] ">Create your account</h1>
         <p className="font-semibold text-[1.05rem] leading-[1.5rem] text-[#36474F] ">
