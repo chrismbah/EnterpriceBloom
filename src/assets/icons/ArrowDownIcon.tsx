99a798fa04ecdd9bd243fa7dff8e8dfd7725c5e8
chrismbah <mbahchris46@gmail.com>
2024-06-07 10:10:16 +0100

    add svg icon

--- src/assets/icons/ArrowDownIcon.tsx
+++ src/assets/icons/ArrowDownIcon.tsx
@@ -1,19 +1,17 @@
-export const ArrowDownIcon = () => {
+export const ArrowDownIcon = ({ className }: { className?: string }) => {
   return (
-    <div>
-      <svg
-        className="w-4 h-4 text-neutral-700"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M19 9l-7 7-7-7"
-        />
-      </svg>
-    </div>
+    <svg
+      className={`w-4 h-4 cursor-pointer ${className}`}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M19 9l-7 7-7-7"
+      />
+    </svg>
   );
 };
