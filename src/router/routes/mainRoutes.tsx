9f7983d0cde488830631035986a1e88b951c0de1
chrismbah <mbahchris46@gmail.com>
2024-06-11 19:31:57 +0100

    add: home page route

--- src/router/routes/mainRoutes.tsx
+++ src/router/routes/mainRoutes.tsx
@@ -1,2 +1,9 @@
+import HomePage from "../../pages/home/HomePage";
 import { IRoutes } from ".";
-export const mainRoutes: IRoutes[] = [];
+export const mainRoutes: IRoutes[] = [
+  {
+    path: "/",
+    element: <HomePage />,
+    authGuard: false,
+  },
+];
