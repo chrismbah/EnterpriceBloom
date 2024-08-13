20238273d7c0c39296f1538d67ca855d7cff52a4
chrismbah <mbahchris46@gmail.com>
2024-08-13 18:33:59 +0100

    push changes 2

--- src/pages/Home/MarketPlace/index.tsx
+++ src/pages/Home/MarketPlace/index.tsx
@@ -45,34 +45,67 @@ const MarketPlace = () => {
       <div className="basis-1/4">
         <SideNavbar />
       </div>
-      <div className="basis-3/4 bg-white rounded-lg py-8 px-7 ">
-        <h1 className="text-[28px] font-bold mb-2">
-          Different services you need
-        </h1>
-        <div className="flex flex-col gap-8">
-          <div className="w-full">
-            <div className="grid grid-cols-1 gap-y-[28px] gap-x-[14px] md:grid-cols-2 lg:grid-cols-3">
-              {marketplace.map(({ id, title, prof_no, img }) => (
-                <div key={id} className="flex flex-col gap-[8px]">
-                  <img
-                    src={img}
-                    alt=""
-                    className="rounded-lg h-[150px] w-full object-cover "
-                  />
-                  <div className="">
-                    <h1 className="font-semibold">{title}</h1>
-                    <p className="text-[#36474F] font-semibold text-xxs">
-                      Over {prof_no.toLocaleString()} Professionals
-                    </p>
+      <div className="flex flex-col gap-2">
+        <div className="basis-3/4 bg-white rounded-lg py-8 px-7 ">
+          <h1 className="text-[28px] font-bold mb-2">
+            Different services you need
+          </h1>
+          <div className="flex flex-col gap-8">
+            <div className="w-full">
+              <div className="grid grid-cols-1 gap-y-[28px] gap-x-[14px] md:grid-cols-2 lg:grid-cols-3">
+                {marketplace.map(({ id, title, prof_no, img }) => (
+                  <div key={id} className="flex flex-col gap-[8px]">
+                    <img
+                      src={img}
+                      alt=""
+                      className="rounded-lg h-[150px] w-full object-cover "
+                    />
+                    <div className="">
+                      <h1 className="font-semibold">{title}</h1>
+                      <p className="text-[#36474F] font-semibold text-xxs">
+                        Over {prof_no.toLocaleString()} Professionals
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+            </div>
+            <div className="w-full flex items-center justify-center">
+              <button className="border border-[#36474F] rounded-[32px] py-2 px-5 text-[#36474F] font-bold text-xs ">
+                See More
+              </button>
             </div>
           </div>
-          <div className="w-full flex items-center justify-center">
-            <button className="border border-[#36474F] rounded-[32px] py-2 px-5 text-[#36474F] font-bold text-xs ">
-              See More
-            </button>
+        </div>
+        <div className="basis-3/4 bg-white rounded-lg py-8 px-7 ">
+          <h1 className="text-[28px] font-bold mb-2">
+            Different services you need
+          </h1>
+          <div className="flex flex-col gap-8">
+            <div className="w-full">
+              <div className="grid grid-cols-1 gap-y-[28px] gap-x-[14px] md:grid-cols-2 lg:grid-cols-3">
+                {marketplace.map(({ id, title, prof_no, img }) => (
+                  <div key={id} className="flex flex-col gap-[8px]">
+                    <img
+                      src={img}
+                      alt=""
+                      className="rounded-lg h-[150px] w-full object-cover "
+                    />
+                    <div className="">
+                      <h1 className="font-semibold">{title}</h1>
+                      <p className="text-[#36474F] font-semibold text-xxs">
+                        Over {prof_no.toLocaleString()} Professionals
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="w-full flex items-center justify-center">
+              <button className="border border-[#36474F] rounded-[32px] py-2 px-5 text-[#36474F] font-bold text-xs ">
+                See More
+              </button>
+            </div>
           </div>
         </div>
       </div>
