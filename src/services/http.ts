93a95ee7d42315a5580fafaa113fa27893e527ef
chrismbah <mbahchris46@gmail.com>
2024-09-17 18:03:13 +0100

    remove env

--- src/services/http.ts
+++ src/services/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const http = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "https://combackend.onrender.com",
   withCredentials: true,
 });
 
--- src/store/slices/apiSlice.ts
+++ src/store/slices/apiSlice.ts
@@ -7,7 +7,7 @@ import {
 } from "@reduxjs/toolkit/query/react";
 import { logout } from "./authSlice";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "https://combackend.onrender.com";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
